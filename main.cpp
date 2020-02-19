b9f87c30338ecffe3adca9035c43fc4669e0ed3f
Rares <rsahleanu@gmail.com>
2020-02-19 21:32:20 +0100

    i

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 int main() {
 
-    std::ifstream  file("data.txt");
+    std::ifstream  file("C:\\Users\\rasah_s2z0ncs\\CLionProjects\\untitled10\\data.txt");
     if(file.good()){
-        while(!file.eof()){
-            char *st;
-           file.getline(st,10);
-           std::cout << st;
-           st = "";
-        }
+        char ch;
+      file.seekg(0, std::ios::end);
+      auto size = file.tellg();
+      std::cout << "DG " << size << "\n";
+      file.seekg(0,std::ios::beg);
+       std::vector<char> puffer(size);
+       file.read(puffer.data(),size);
+       std::cout<< "gelesen: " << file.gcount() << "\n";
+        std::cout<< "text: " << puffer.data();
     }
 }
